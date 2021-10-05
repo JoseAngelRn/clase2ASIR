992d4cc1ec15077145b18678106a555e606bcbbb
JoseAngelRn <joseangel.ma44965@cesurformacion.com>
2021-10-05 08:30:54 +0200

    ejercicio mostrar dia y el horario

--- Horario.php
+++ Horario.php
@@ -5,6 +5,18 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <head>
+            <style>
+            table,
+            th,
+            td {
+            border: 5px solid lightseagreen;
+            width: auto;
+            border-collapse: collapse;
+            background: lightgoldenrodyellow;
+            color: black;
+            }
+            </style>
 </head>
 <body>
     <?php
@@ -13,35 +25,137 @@
     $dia = date("l");
 
     switch ($dia) {
-        case 'Lunes':
-            echo $dia."<br>";
-            echo "El horario de hoy es:"."<br>"
+        case 'Monday':
+            echo "El dia de hoy es: ".$dia."<br>";
+            echo "Y el horario es:
+            <body>
+            <table>
+            <tr>
+                <td>Aplicaciones web</td>
+            </tr>
+            <tr>
+                <td>Aplicaiones web</td>
+            </tr>
+            <tr>
+                <td>Aplicaciones web</td>
+            </tr>
+            <tr>
+                <td>Servios en red</td>
+            </tr>
+            <tr>
+                <td>Servicios en red</td>
+            </tr>
+            <tr>
+                <td>Servicios en red</td
+            </tr>
+            </table>"."<br>";
             break;
 
-        case 'Martes':
-            echo $dia."<br>";
-            echo "El horario de hoy es: "."<br>";
+        case 'Tuesday':
+            echo "El dia de hoy es: ".$dia."<br>";
+            echo "Y el horario es:  
+            <table>
+            <tr>
+                <td>Administracion de S.O</td>
+            </tr>
+            <tr>
+                <td>Administracion de S.O</td>
+            </tr>
+            <tr>
+                <td>Administracion de S.O</td>
+            </tr>
+            <tr>
+                <td>Seguridad informatica</td>
+            </tr>
+            <tr>
+                <td>Empresas</td>
+            </tr>
+            <tr>
+                <td>Empresas</td>
+            </tr>
+            </table>"."<br>";
             break;
 
-        case 'Miercoles':
-            echo $dia."<br>";
-            echo "El horario de hoy es: "."<br>";
+        case 'Wednesday':
+            echo "El dia de hoy es: ".$dia."<br>";
+            echo "Y el horario es: 
+            <table>
+            <tr>
+                <td>Administracion de S.O</td>
+            </tr>
+            <tr>
+                <td>Administracion de S.O</td>
+            </tr>
+            <tr>
+                <td>Administracion de S.O</td>
+            </tr>
+            <tr>
+                <td>Servicios en red</td>
+            </tr>
+            <tr>
+                <td>Servicios en red</td>
+            </tr>
+            <tr>
+                <td>Servicios en red</td>
+            </tr>
+            </table>"."<br>";
             break;
 
-        case 'Jueves':
-            echo $dia."<br>";
-            echo "El horario de hoy es: "."<br>";
+        case 'Thursday':
+            echo "El dia de hoy es: ".$dia."<br>";
+            echo "Y el horario es:  
+            <table>
+            <tr>
+                <td>Base de datos</td>
+            </tr>
+            <tr>
+                <td>Base de datos</td>
+            </tr>
+            <tr>
+                <td>Base de datos</td>
+            </tr>
+            <tr>
+                <td>Seguridad informatica</td>
+            </tr>
+            <tr>
+                <td>Ingles</td>
+            </tr>
+            <tr>
+                <td>Ingles</td>
+            </tr>
+            </table>"."<br>";
             break;
 
-        case 'Viernes':
-            echo $dia."<br>";
-            echo "El horario de hoy es: "."<br>";
+        case 'Friday':
+            echo "El dia de hoy es: ".$dia."<br>";
+            echo "Y el horario es:  
+            <table>
+            <tr>
+                <td>Ingles</td>
+            </tr>
+            <tr>
+                <td>Seguridad informatica</td>
+            </tr>
+            <tr>
+                <td>Seguridad informatica</td>
+            </tr>
+            <tr>
+                <td>Aplicaciones web</td>
+            </tr>
+            <tr>
+                <td>Empresas</td>
+            </tr>
+            <tr>
+                <td>Empresas</td>
+            </tr>
+            </table>"."<br>";
             break;
 
         default:
-        echo $dia."<br>";
+        echo "El dia de hoy es: ".$dia."<br>";
             break;
     }
     ?>
+     <a href="paginainicio.html"> Volver atras</a>
 </body>
 </html>
