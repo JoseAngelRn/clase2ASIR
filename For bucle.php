8b3bd3de170a442dac3c5c5f88f5b4783bb7d624
JoseAngelRn <joseangel.ma44965@cesurformacion.com>
2021-10-11 09:51:01 +0200

    Bucleeeeeee

---
+++ For bucle.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    echo "<h4>Ejemplo de bucle For</h4>"."<br>";
+
+    for ($cuenta = 0; $cuenta < 10 ; $cuenta ++) {
+        #Paso 1: Acción
+        echo "Muestra la cuenta: ".$cuenta."<br>";
+}
+    echo "<br>"."<br>";
+
+
+#EJEMPLO DE FUNCIÓN
+    $esMayor = true;
+
+    function persona ($nombre, $edad){
+        echo "Su nombre es: ".$nombre."<br>";
+    if ($edad % 2 == 0) {
+        echo "Su edad es: par"."<br>";
+    }
+    else {
+        echo"Su edad es: impar"."<br>";
+    }
+}
+#LLAMAMOS A LA FUNCIÓN
+    persona("Manulon", 22);
+    persona("Manulen", 91);
+    persona("Manulin", 27);
+    persona("Manule", 45);
+    
+    echo "<br>"."<br>";
+#EJEMPLO DE ARRAY
+    $alumnos = array("Inachio","Paaaco","Mígue","ADAM","Manule","Ívam");
+        echo "Hay ".count($alumnos)." alumnos"."<br>";
+    $selecAleatorio = rand (0 , 5);
+
+    echo "El alumno es: ".$alumnos[$selecAleatorio];
+
+    ?>
+</body>
+</html>
---
+++ while bucle.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+
+    echo "<h4>Ejemplo de bucle while</h4>"."<br>";
+    $numero = 0;
+    
+    while ($numero <= 10) {
+        #Paso 1: mostrar mi número
+        echo "Mi número es: ".$numero."<br>";
+        #Paso 2: Actualizar la variable número
+        $numero++;
+    }
+    echo "<strong>Mi cuenta ha terminado.</strong>"."<br>";
+
+
+    echo "<h4>Ejemplo de bucle while de 5 en 5</h4>"."<br>";
+    $numero1 = 0;
+    
+    while ($numero1 <= 100) {
+        #Paso 1: mostrar mi número
+        echo "Mi número es: ".$numero1."<br>";
+        #Paso 2: Actualizar la variable número
+        $numero1+=5; #$numero1=$numero1+5;
+    }
+    echo "<strong>Mi cuenta ha terminado.</strong>"."<br>";
+
+
+    echo "<h4>Ejemplo de bucle while numero aleatorios</h4>"."<br>";
+    $numero2 = 0;
+    $aleatorio = rand(0,100);
+    
+    while ($numero2 <= $aleatorio) {
+        #Paso 1: mostrar mi número
+        echo "Mi número aleatorio es: ".$aleatorio."<br>";
+        echo "Mi número es: ".$numero2."<br>";
+        #Paso 2: Actualizar la variable número
+        $numero2++;
+    }
+    echo "<strong>Mi cuenta ha terminado.</strong>"."<br>"
+    ?>
+</body>
+</html>
