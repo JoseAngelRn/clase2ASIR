95e2437a142ce0b965729f04d5e5b3c380cbf213
JoseAngelRn <joseangel.ma44965@cesurformacion.com>
2021-10-18 09:46:30 +0200

    raiz, elevar y fibonacci

--- Calculadora.php
+++ Calculadora.php
@@ -40,6 +40,10 @@
                     <button name="" type="button">x</button>
                     <button name="" type="button">/</button>
                     <button name="" type="button">%</button>
+                    <button name="" type="button">√</button>
+                    <button name="" type="button">x²</button>
+                    <button name="" type="button">x³</button>
+                    <button name="" type="button">xⁿ</button>
 
                 </br></br></br>
                     <input name="" type="submit" value="Resolver">
@@ -79,17 +83,59 @@ function modulo($num1,$num2){
         print "El resultado es: ".$res."</br>";
     }
 
+#FUNCIÓN RAÍZ CUADRADA
+    function raiz_cuadrada($num){
+        $res = sqrt($num);
+            print "El resultado es: ".$res."</br>";
+        }
+
+#FUNCIÓN ELEVAR AL CUADRADO
+    function elevar_cuadrado($num){
+        $res = $num * $num;
+            print "El resultado es: ".$res."</br>";
+        }
+
+#FUNCIÓN ELEVAR AL CUBO
+    function elevar_cubo($num){
+        $res = $num * $num * $num;
+            print "El resultado es: ".$res."</br>";
+        }
+
+#UNCIÓN ELEVAR A CUALQUIER EXPONENTE
+        function elevar_exponente($base,$exp){
+            $res = pow($base , $exp); 
+            print "El resultado es: ".$res."</br>";
+        }
+
+#FUNCÓN FIBONACCI
+    function fibonacci($final){
+        $contador = 1;
+        $num1 = 0;
+        $num2 = 1;
+        $num3 = 0;
+
+    while ($contador <= $final) {
+        $num3 = $num1 + $num2;
+        $num1 = $num2;
+        $num2 = $num3;
+        echo "El resultado es: ".$num3."</br>";
+        $contador ++;
+        }
+    }
 
 $prueba1=10;
 $prueba2=2;
+$prueba3=20;
 suma($prueba1,$prueba2);
 resta($prueba1,$prueba2);
 multiplicacion($prueba1,$prueba2);
 division($prueba1,$prueba2);
 modulo($prueba1,$prueba2);
-
-
-
+raiz_cuadrada($prueba3);
+elevar_cuadrado($prueba1);
+elevar_cubo($prueba1);
+elevar_exponente($prueba1,$prueba2);
+fibonacci($prueba3);
 ?>
 </body>
 </html>
