651f6d2d8def113eefb95b3bf721c4f92ebad3da
JoseAngelRn <joseangel.ma44965@cesurformacion.com>
2021-11-15 10:49:05 +0100

    Terminado faltan algunos retoqus de cs

--- Gestion-De-Vuelos.php
+++ Gestion-De-Vuelos.php
@@ -1,7 +1,7 @@
 <?php
 
 #Recogida de variables
-if(isset($_POST["vuelo1"]) && isset($_POST["vuelo1"])){
+if(isset($_POST["vuelo1"]) && isset($_POST["vuelo2"])){
     $selector = $_POST["opciones"];
 
     $vuelo1 = $_POST["vuelo1"];
@@ -19,22 +19,23 @@ if(isset($_POST["vuelo1"]) && isset($_POST["vuelo1"])){
         $fechasalida2 = $vuelo2["FechaSalida2"];
 
 #Función unir vuelos
-function combinar($vuelo1,$vuelo2){
+function Combinar($vuelo1,$vuelo2){
     $Combinar_Vuelos= array_merge($vuelo1,$vuelo2);
         echo "<b>vuelos unidos: </b></br>";
         print_r($Combinar_Vuelos);
 }
 #Diferencias entre vuelos
-function diferencias($vuelo1,$vuelo2){
+function Diferencias($vuelo1,$vuelo2){
     $Diferecias_Vuelos= array_diff($vuelo1,$vuelo2);
         echo "<b>Diferencias entre vuelos: </b></br>";
         print_r($Diferecias_Vuelos);
 }
 #Ordena vuelos
-function ordenar($vuelo1){
-    $Ordena_Vuelos= sort($vuelo1);
+function Ordenar($vuelo1,$vuelo2){
+    $Combinar_Vuelos2= array_merge($vuelo1,$vuelo2);
+    $Ordena_Vuelos= sort($Combinar_Vuelos2);
         echo "<b>Datos del vuelo ordenado: </b></br>";
-        print_r($vuelo1);
+        print_r($Combinar_Vuelos2);
 }
 #Información común
 function InfoComun($vuelo1,$vuelo2){
@@ -47,19 +48,19 @@ function InfoComun($vuelo1,$vuelo2){
 #SELECTOR
 switch ($selector) {
     case 'CombVuelos':
-        function combinar($vuelo1,$vuelo2);
+        Combinar($vuelo1,$vuelo2);
         break;
 
     case 'DifVuelos':
-        function diferencias($vuelo1,$vuelo2);
+        Diferencias($vuelo1,$vuelo2);
         break;
 
     case 'OrdVuelos':
-        function ordenar($vuelo1);
+        Ordenar($vuelo1,$vuelo2);
         break;
 
     case 'InfoComun':
-        function InfoComun($vuelo1,$vuelo2);
+        InfoComun($vuelo1,$vuelo2);
         break;
 
     default:
