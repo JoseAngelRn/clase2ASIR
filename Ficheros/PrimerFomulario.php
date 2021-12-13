1bbd7bc0238c69ba69ff209f918e697fe91afa03
JoseAngelRn <joseangel.ma44965@cesurformacion.com>
2021-12-13 10:48:15 +0100

    como hacer un salto de linea o espacio en blanco

--- Ficheros/PrimerFomulario.php
+++ Ficheros/PrimerFomulario.php
@@ -6,11 +6,10 @@ $opcion = $_GET["opcion"];
 if ($opcion <= 1) {
 $Fopen = fopen("ficherosprimerFormulario.txt","a") or die ("ERROR");
 
-fwrite($Fopen, $texto);
+fwrite($Fopen, $texto. "<br>"); #concatenando un br hacemos un salto de linea, si concatenamos un \n hacemos un espacio
 fclose($Fopen);
 echo "Se añadió el texto al fichero";
 }
-
 else{
     echo "Este es el contenido del documento:"."<br>";
     $Fopen = fopen("ficherosprimerFormulario.txt","r") or die ("ERRROR");
