6f1618b49e7169212018f2f310fe378ad2567f88
JoseAngelRn <joseangel.ma44965@cesurformacion.com>
2022-01-10 09:12:41 +0100

    define

---
+++ Constantes/Constantes.php
@@ -0,0 +1,19 @@
+<?php
+
+#Constantes
+
+$pi = pi();
+
+echo "Numero Pi: ". $pi."<br>";
+
+define ("GRAVEDAD",9.8); #Para que nadie pueda cambiar la variable
+
+echo "Constante de la gravedad: ".GRAVEDAD;
+
+
+
+
+
+
+
+?>
