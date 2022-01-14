0f37508a4554540c303c3f2fb2736bb57ea58aa9
JoseAngelRn <joseangel.ma44965@cesurformacion.com>
2022-01-14 12:21:23 +0100

    Include y require

---
+++ Constantes/include2.php
@@ -0,0 +1,12 @@
+<?php
+
+define ("GRAVEDAD",9.8);
+
+function sumar($Numero1, $Numero2){
+    $res = $Numero1 + $Numero2;
+    echo $res;
+}
+
+
+
+?>
---
+++ Constantes/includes.php
@@ -0,0 +1,11 @@
+<?php
+#Los 2 enlazan otro archivo pero el require es más restrictivo
+INCLUDE "include2.php";
+# REQUIRE "include2.php";
+
+
+echo GRAVEDAD;
+echo "<br>";
+echo sumar(1, 2);
+
+?>
