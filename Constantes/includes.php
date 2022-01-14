8aeacb40c797aaa940ad29308d93c19f1a2e08e6
JoseAngelRn <joseangel.ma44965@cesurformacion.com>
2022-01-14 12:37:26 +0100

    INCLUDE, REQUIRE, INCLUDE_ONCE

--- Constantes/includes.php
+++ Constantes/includes.php
@@ -1,11 +1,17 @@
 <?php
-#Los 2 enlazan otro archivo pero el require es más restrictivo
-INCLUDE "include2.php";
+# Los 2 enlazan otro archivo pero el require es más restrictivo
+# INCLUDE "include2.php";
 # REQUIRE "include2.php";
+INCLUDE_ONCE "include2.php"; # El INCLUDE_ONCE es lo mismo pero solo muestra una vez el error
 
 
 echo GRAVEDAD;
 echo "<br>";
 echo sumar(1, 2);
+
+
+resta();
+
+
 
 ?>
