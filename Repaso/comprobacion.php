a186ca23b9047599130bf1d2f5895407064b8307
JoseAngelRn <joseangel.ma44965@cesurformacion.com>
2022-02-07 10:04:42 +0100

    casi terminado la comprobacion

--- Repaso/comprobacion.php
+++ Repaso/comprobacion.php
@@ -1,9 +1,16 @@
 <?php
 
-if (empty($_POST)) {
-    
+# Campos requeridos
+if (! empty($_POST)) {
+    if (isset($_POST["Nombre"]) && isset($_POST["Apellidos"])) {
+        echo "Por favor, introduzca información. Nombre y apellidos";
+    } else {
+        
+        echo "Nombre: ".$_POST["Nombre"]."<br>";
+        echo "Apellidos: ".$_POST["Apellidos"]."<br>";
+    }
 } else {
-    echo "Por favor rellena los campos y reicia formulario vacio";
+    echo "Por favor, introduzca información. Reinicia";
 }
 
 
