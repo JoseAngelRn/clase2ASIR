8594645d38c6c6c8f917e33f2fa6f4593c71fe26
JoseAngelRn <joseangel.ma44965@cesurformacion.com>
2022-02-07 10:09:17 +0100

    comprobar si esta el fichero

---
+++ Repaso/fichero_repaso_fopen.php
@@ -0,0 +1,12 @@
+<?php
+
+#Comprobación de si existe el fichero
+
+if (file_exists("pruebafichero.txt") == true) {
+    echo "El fichero existe";
+} else {
+    echo "No está el fichero";
+}
+
+
+?>
